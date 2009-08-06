23010e5d8d1214701e04a0c3fa16aa61885f6b71
maclean <gilleain.torrance@gmail.com>
2009-08-06 13:31:56 +0100

    utility method to print out the bond list

--- src/test/signature/TestAtomContainerAtomPermutor.java
+++ src/test/signature/TestAtomContainerAtomPermutor.java
@@ -2,6 +2,7 @@ package test.signature;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IBond;
 import org.openscience.cdk.isomorphism.UniversalIsomorphismTester;
@@ -13,6 +14,28 @@ public class TestAtomContainerAtomPermutor {
     
     private static NoNotificationChemObjectBuilder builder =
         NoNotificationChemObjectBuilder.getInstance();
+    
+    public static void printContainer(IAtomContainer container) {
+        for (IAtom atom : container.atoms()) {
+            System.out.print(atom.getSymbol());
+        }
+        System.out.print(" ");
+        int i = 0;
+        for (IBond bond : container.bonds()) {
+            int a1 = container.getAtomNumber(bond.getAtom(0));
+            int a2 = container.getAtomNumber(bond.getAtom(1));
+            if (a1 < a2) {
+                System.out.print(String.format("%s-%s", a1, a2));
+            } else {
+                System.out.print(String.format("%s-%s", a2, a1));
+            }
+            if (i < container.getBondCount() - 1) {
+                System.out.print(".");
+            }
+            i++;
+        }
+        System.out.print(" ");
+    }
     
     @Test
     public void testPermute() throws Exception {
@@ -33,6 +56,7 @@ public class TestAtomContainerAtomPermutor {
             IAtomContainer permutedContainer = permutor.next();
             boolean isomorphic = UniversalIsomorphismTester.isIsomorph(
                     square, permutedContainer);
+            printContainer(permutedContainer);
             Assert.assertTrue(isomorphic);
         }
     }
